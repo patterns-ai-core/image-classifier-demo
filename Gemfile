623d7fa16177362becd1929a37c9a302866ba284
Andrei Bondarev <andrei@sourcelabs.io>
2024-10-13 20:05:23 -0400

    Add files via upload

---
+++ Gemfile
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+ruby "3.3.4"
+
+source "https://rubygems.org"
+
+gem "langchainrb"
+gem "ruby-openai"
+gem "pry-byebug"
+gem "dotenv"
---
+++ image-classifier.rb
@@ -0,0 +1,54 @@
+require "langchain"
+
+class ImageClassifier
+  extend Langchain::ToolDefinition
+
+    define_function :classify, description: "Classify photo" do
+      property :main_subject, 
+        description: "Primary subject of the photo",
+        type: "string",
+        required: true
+      property :scene_type,
+        description: "Scene setting in the photo",
+        type: "string",
+        required: true,
+        enum: ["urban", "rural", "indoor", "outdoor"]
+      property :colors,
+        description: "Dominant colors in the photo",
+        type: "array",
+        required: true do
+          item type: "string"
+        end
+      property :objects,
+        description: "List of key objects visible in the image",
+        type: "array",
+        required: true do
+          item type: "string"
+        end
+      property :actions,
+        description: "Activities or movements captured",
+        type: "array",
+        required: true do
+          item type: "string"
+        end
+      property :number_of_humans,
+        description: "Number of people if present",
+        type: "number",
+        required: true
+      property :weather_conditions,
+        description: "Weather conditions",
+        type: "string",
+        required: true,
+        enum: ["sunny", "cloudy", "rainy", "snowy", "foggy"]
+      property :time_of_day,
+        description: "Time of day",
+        type: "string",
+        required: true,
+        enum: ["morning", "afternoon", "evening", "night"]
+      property :mood,
+        description: "Overall feeling conveyed by the photo",
+        type: "string",
+        required: true
+  end
+end
+
